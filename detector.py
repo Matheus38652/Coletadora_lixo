1c6110724c4065db72de3643b2b1318c97fb8347
Matheus38652 <silva.santos13652@gmail.com>
2025-10-17 17:42:29 -0300

    alterado caminho de busca do treinamento

---
+++ detector.py
@@ -0,0 +1,29 @@
+import cv2
+from ultralytics import YOLO
+
+model_path = 'runs/detect/treino_lixo/weights/best.pt'
+model = YOLO(model_path)
+
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Erro: Não foi possível abrir a câmera.")
+    exit()
+
+while True:
+    success, frame = cap.read()
+
+    if success:
+        results = model(frame)
+
+        annotated_frame = results[0].plot()
+
+        cv2.imshow("Detector de Lixo", annotated_frame)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    else:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
